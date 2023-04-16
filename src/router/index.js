e390b0b2403d8ed671f50c0fc21beacf8c17a334
Leon Chou <iamyiii0328@gmail.com>
2023-04-17 00:22:48 +0800

    Add new vue router path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@pages/Home.vue";
 import Articles from "@pages/Articles.vue";
+import NewPost from "@pages/NewPost.vue";
+import Login from "@pages/Login.vue";
 
 const routes = [
   {
@@ -13,6 +15,16 @@ const routes = [
     path: "/articles",
     name: "Articles",
     component: Articles,
+  },
+  {
+    path: "/newpost",
+    name: "NewPost",
+    component: NewPost,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
