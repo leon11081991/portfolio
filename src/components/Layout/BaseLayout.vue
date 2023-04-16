8d08885f4cf3ac345fc08aac8789592207277312
Leon Chou <iamyiii0328@gmail.com>
2023-04-16 22:59:24 +0800

    component basic layout template

---
+++ src/components/Layout/BaseLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="web_main">
+    <slot></slot>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.web_main {
+  height: 800px;
+}
+</style>
