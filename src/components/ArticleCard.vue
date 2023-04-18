692a7852cdd9869006ce69a279eba082829376ef
Leon Chou <iamyiii0328@gmail.com>
2023-04-18 22:50:54 +0800

    component- articleCard

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="articles-item">
+    <a class="articles-item-inner">
+      <div class="card-top"></div>
+      <h4 class="card-title">article card</h4>
+      <div class="card-desc">
+        <p></p>
+      </div>
+    </a>
+  </li>
+</template>
+
+<script></script>
+
+<style lang="scss"></style>
