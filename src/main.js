f31e558b6e6c0bc89fe47cb93ab5400418ec47fb
Leon Chou <iamyiii0328@gmail.com>
2023-04-18 22:46:53 +0800

    Threejs- main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import "./scss/styles.scss";
 import { createApp } from "vue";
-
+import "./js/three"; // three.js設定
+//import "./js/threeSpotLine"; // three.js設定
 import App from "./App.vue";
 import { router } from "./router/index";
 
