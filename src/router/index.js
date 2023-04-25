6be3c28490cb3a776fd035685dabd03285c02863
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 19:37:17 +0800

    router - add meta title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { store } from "../store/index";
 
 import Home from "@pages/Home.vue";
+import About from "@pages/About.vue";
 import Articles from "@pages/Articles.vue";
 import NewPost from "@pages/NewPost.vue";
 import Login from "@pages/Login.vue";
@@ -10,27 +12,55 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    meta: {
+      title: "About Me",
+    },
   },
   {
     path: "/articles",
     name: "Articles",
     component: Articles,
+    meta: {
+      title: "Articles",
+    },
   },
   {
     path: "/newpost",
     name: "NewPost",
     component: NewPost,
+    meta: {
+      title: "New Post",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  // 設定頁面標籤title name
+  document.title = `${to.meta.title} | LEON`;
+
+  // 跳轉頁面時，將 menu 收合
+  store.state.header.menu = false;
 });
 
 export { router };
