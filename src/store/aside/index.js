20d50250c6d3d76770cc396ee3b219c76be7abc9
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 17:52:12 +0800

    Vuex - aside's

---
+++ src/store/aside/index.js
@@ -0,0 +1,27 @@
+export const aside = {
+  state() {
+    return {
+      socialMedia: [
+        {
+          name: "github",
+          link: "https://github.com/leon11081991",
+        },
+        {
+          name: "linkin",
+          link: "https://www.linkedin.com/in/yi-ting-chou-78653a270/",
+        },
+        {
+          name: "notion",
+          link: "https://www.linkedin.com/in/yi-ting-chou-78653a270/",
+        },
+        {
+          name: "codepen",
+          link: "https://www.linkedin.com/in/yi-ting-chou-78653a270/",
+        },
+      ],
+    };
+  },
+  mutations: {},
+  getters: {},
+  actions: {},
+};
