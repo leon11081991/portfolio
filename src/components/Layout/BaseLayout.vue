3197f15545d8f0ffe49d68d57992747972cafab5
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 18:53:53 +0800

    component - BaseLayout

--- src/components/Layout/BaseLayout.vue
+++ src/components/Layout/BaseLayout.vue
@@ -1,15 +1,26 @@
 <template>
   <main class="web_main">
+    <Header />
+    <Aside />
     <slot></slot>
-    <Footer></Footer>
+    <Footer />
   </main>
 </template>
 
 <script setup>
+// import
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import Header from "@components/Header/Header.vue";
+import Aside from "@components/Aside/Aside.vue";
 import Footer from "@components/Footer/Footer.vue";
+
+// data
+const store = useStore();
 </script>
 
 <style lang="scss" scoped>
 .web_main {
+  position: relative;
 }
 </style>
