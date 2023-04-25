44d97e5670f607d5bdede50a672f65b8e9c6cef6
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 18:59:33 +0800

    component - HomeSection edit

--- src/components/Layout/HomeSection.vue
+++ src/components/Layout/HomeSection.vue
@@ -1,70 +1,127 @@
 <template>
   <section class="home-section" :class="sectionClass">
-    <div :class="sectionClass + '-wrapper'" class="container">
+    <div :class="sectionClass + '-wrapper page-container'">
+      <!-- Hero Section Start -->
       <template v-if="section.name == 'hero'">
-        <span class="locate">
-          <location class="icon" />
-          {{ section.locate }}
-        </span>
-        <span class="intro">{{ section.intro }}</span>
-        <h2 class="title">{{ section.title }}</h2>
-        <p class="subtitle">{{ section.subTitle }}</p>
-        <p class="desc">{{ section.desc }}</p>
+        <div class="section-content">
+          <Dot class="dot" />
+          <span class="intro">{{ section.intro }}</span>
+          <h2 class="title">{{ section.title }}</h2>
+          <p class="subtitle">{{ section.subTitle }}</p>
+          <p class="desc">{{ section.desc }}</p>
+          <router-link to="#" class="btn readMore">
+            Read More
+            <span class="arrow"></span>
+            <div class="circle-wrapper">
+              <Circle />
+            </div>
+          </router-link>
+        </div>
+        <div class="section-image">
+          <div class="img_div">
+            <img src="/img/hero.jpg" alt="" />
+          </div>
+          <div class="img_bg"></div>
+        </div>
       </template>
+      <!-- Hero Section End -->
+
+      <!-- About Section Start -->
       <template v-if="section.name == 'about'">
         <h3 class="title">{{ section.title }}</h3>
       </template>
+      <!-- About Section End -->
     </div>
   </section>
 </template>
 
 <script setup>
+// import
 import { ref } from "vue";
-import location from "@assets/icons/location.svg";
+import Dot from "@assets/icons/dot.svg";
+import Circle from "@assets/icons/circle.svg";
+
+// data
 const props = defineProps(["section"]);
-console.log(props.section);
 const sectionClass = ref(props.section.name);
 </script>
 
 <style lang="scss" scoped>
+$hero_img-w: 636;
+$hero_img-h: 437;
 .home-section {
   position: relative;
+  padding-top: 104px;
   padding-bottom: 104px;
   &.hero {
-    height: calc(100vh - 96px);
-    padding-bottom: 0;
-    display: flex;
-    align-items: center;
+    padding-top: 93px;
     .hero-wrapper {
-    }
-    .locate {
+      position: relative;
+      height: 100%;
       display: flex;
+      justify-content: space-between;
       align-items: center;
-      font-size: 0.8rem;
-      .icon {
-        padding-right: 0.5rem;
+    }
+    .section {
+      &-content {
+        position: relative;
+        width: calc(467 / 1240) * 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        @include media-breakpoint-down(lg) {
+          width: inherit;
+        }
       }
+      &-image {
+        position: relative;
+        width: calc($hero_img-w / 1240) * 100%;
+        &:hover {
+          .img_div {
+            transform: translate(0, 0);
+          }
+        }
+
+        .img_div {
+          padding-bottom: calc($hero_img-h / $hero_img-w) * 100%;
+          filter: grayscale(95%);
+          z-index: 1;
+          transform: translate(-22px, -22px);
+          transition: transform 0.3s ease-in;
+        }
+        .img_bg {
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          width: 100%;
+          height: 100%;
+          background: $primary;
+        }
+      }
+    }
+    .dot {
+      position: relative;
+      margin-bottom: 40px;
     }
     .intro {
       display: block;
+      margin-bottom: 16px;
+      @extend .txt-headline3;
     }
     .title {
-      //text-align: center;
-      font-size: clamp(40px, 10vw, 100px);
-      font-weight: 900;
-      letter-spacing: 0.1rem;
-      color: $focus;
-    }
-    .subtitle {
-      //text-align: center;
-      font-size: clamp(32px, 10vw, 60px);
-      line-height: 1;
-      font-weight: 600;
+      @extend .txt-headline1;
+      //font-size: clamp(40px, 10vw, 100px);
+      text-transform: uppercase;
+      //color: $primary;
     }
     .desc {
-      margin-top: 2rem;
-      width: 60%;
+      @extend %base-text-style;
+      margin-top: 16px;
+      margin-bottom: 40px;
     }
+  }
+  &.about {
   }
 }
 </style>
