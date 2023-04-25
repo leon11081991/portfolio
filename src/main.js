ccedbb04d496afc5f3cbfeb3b8234bf9d00aa506
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 18:58:32 +0800

    Edit main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // import "./scss/styles.scss";
 import { createApp } from "vue";
-import "./js/three"; // three.js設定
-//import "./js/threeSpotLine"; // three.js設定
+// import "./js/three"; // three.js設定
 import App from "./App.vue";
 import { router } from "./router/index";
+import { store } from "./store/index";
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 
 app.mount("#app");
