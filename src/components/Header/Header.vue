c5f4b2cb6492cbb080e3f4196c079dc08de618a5
Leon Chou <iamyiii0328@gmail.com>
2023-04-25 18:50:30 +0800

    component - Header (重新更改架構, Menu)

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <header class="web_header">
-    <div class="web_header__inner">
-      <div class="logo-box">
+    <div class="web_header__inner container">
+      <!-- Logo Start -->
+      <div class="logo-wrapper">
         <h1 class="logo">
           <router-link class="siteName" to="#">
             <Logo class="logoIcon" />
@@ -9,79 +10,109 @@
           </router-link>
         </h1>
       </div>
+      <!-- Logo End -->
 
-      <div class="carousel-box"></div>
-      <div class="menu" @click="toggleMenu">
-        <span></span>
+      <!-- Marquee Start -->
+      <div class="marquee-wrapper">
+        <ul class="marquee">
+          <li
+            class="marquee-txt"
+            v-for="(marqueeText, index) in marqueeTexts"
+            :key="index"
+          >
+            {{ marqueeText }}
+          </li>
+        </ul>
       </div>
-
-      <Transition name="Menu">
-        <nav class="web_nav" v-if="menu">
-          <ul class="web_nav__list">
-            <li class="web_nav__item">
-              <router-link class="web_nav__item-link" to="#">Info</router-link>
-            </li>
-            <li class="web_nav__item">
-              <router-link class="web_nav__item-link" to="#"
-                >Articles</router-link
-              >
-            </li>
-            <li class="web_nav__item">
-              <router-link class="web_nav__item-link" to="#"
-                >Create Post</router-link
-              >
-            </li>
-            <li class="web_nav__item">
-              <router-link class="web_nav__item-link" to="#"
-                >Login/Register</router-link
-              >
-            </li>
-          </ul>
-        </nav>
-      </Transition>
-      <div class="menu-open-bar" :class="menuBar ? 'toggle' : ''"></div>
+      <!-- Marquee End -->
     </div>
+    <!-- Menu Start -->
+    <div
+      class="menu"
+      :class="store.state.header.menu ? 'active' : ''"
+      @click="store.dispatch('toggleMenu')"
+    >
+      <span></span>
+    </div>
+    <!-- <div
+      class="menu-open-bar"
+      :class="store.state.header.menuBar ? 'toggle' : ''"
+    ></div> -->
+    <!-- Menu End -->
+    <Transition name="Menu">
+      <nav class="web_nav" v-if="store.state.header.menu">
+        <div class="web_nav__inner">
+          <div class="web_nav-main">
+            <ul class="web_nav-list">
+              <li
+                class="web_nav-item"
+                v-for="(navigator, index) in store.state.header.navigationList"
+                :key="index"
+              >
+                <router-link class="web_nav-item-link" :to="navigator.path">
+                  {{ navigator.name }}
+                  <Arrow class="arrow" />
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="web_nav-submain">
+            <div class="socialMedia">
+              <ul class="socialMedia-list">
+                <li class="socialMedia-item">
+                  <a href="" class="socialMedia-item-link" target="_blank">
+                    <Github class="item-icon" />
+                  </a>
+                </li>
+                <li class="socialMedia-item">
+                  <a href="" class="socialMedia-item-link" target="_blank">
+                    <Linkin class="item-icon" />
+                  </a>
+                </li>
+                <li class="socialMedia-item">
+                  <a href="" class="socialMedia-item-link" target="_blank">
+                    <Codepen class="item-icon" />
+                  </a>
+                </li>
+                <li class="socialMedia-item">
+                  <a href="" class="socialMedia-item-link" target="_blank">
+                    <Notion class="item-icon" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="login">
+              <router-link to="/login" class="login-link"
+                >Login / Register</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </nav>
+    </Transition>
   </header>
 </template>
 
 <script setup>
 // import
-import { ref, Transition } from "vue";
+import { ref, onMounted, computed, Transition } from "vue";
+import { useStore } from "vuex";
 import Logo from "@assets/logo.svg";
+import Github from "@assets/icons/github.svg";
+import Linkin from "@assets/icons/linkin.svg";
+import Codepen from "@assets/icons/codepen.svg";
+import Notion from "@assets/icons/notion.svg";
+import Arrow from "@assets/icons/arrow.svg";
 
 // data
-const mobile = ref(null);
-const menu = ref(false);
-const menuBar = ref(false);
-const windowWidth = ref(null); // 視窗寬度
+const store = useStore();
+const marqueeTexts = store.state.header.marqueeTexts;
 
 // methods
-// 偵測視窗寬度
-const checkScreen = () => {
-  windowWidth.value = window.innerWidth;
 
-  // 視窗小於768,啟用手機設定
-  if (windowWidth.value <= 768) {
-    mobile.value = true;
-    console.log("Mobile");
-    return;
-  }
-  mobile.value = false;
-  console.log("Desktop");
-  return;
-};
-window.addEventListener("resize", checkScreen); // 偵測當前視窗寬度
-checkScreen(); // 初始偵測當前視窗寬度
-
-// 點擊Menu
-const toggleMenu = () => {
-  menu.value = !menu.value;
-  menuBar.value = true;
-  // 1秒後移除
-  setTimeout(() => {
-    menuBar.value = false;
-  }, 1000);
-};
+onMounted(() => {
+  console.log("onMounted");
+});
 </script>
 
 <style lang="scss" scoped>
@@ -90,12 +121,15 @@ const toggleMenu = () => {
   top: 0;
   left: 0;
   width: 100%;
-  height: $header-h * 1px;
-  z-index: 99;
+  height: $header_h-desktop * 1px;
+  z-index: $header_zIndex;
   background-color: $white;
   &__inner {
     position: relative;
     height: 100%;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: calc(251 / 1265) * 100% calc(1013 / 1265) * 100%;
   }
   &::after {
     content: "";
@@ -107,65 +141,106 @@ const toggleMenu = () => {
   }
 }
 .logo {
-  &-box {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 328px;
-    height: $header-h * 1px;
+  &-wrapper {
+    position: relative;
+    height: $header_h-desktop * 1px;
     display: flex;
     align-items: center;
-    padding-left: 132px;
   }
-  //display: flex;
-  //align-items: center;
   .logoIcon {
     margin-right: 6px;
   }
   .siteName {
-    @extend %base-text-style;
+    @extend .txt-headline3;
     color: $primary;
   }
 }
+.marquee {
+  &-wrapper {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+    background: $white;
+  }
+  position: absolute;
+  width: 200%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  color: $primary;
+  animation: marquee 20s linear infinite;
+  -webkit-animation: marquee 20s linear infinite;
+
+  &:hover {
+    animation-play-state: paused;
+  }
+
+  &-txt {
+    white-space: nowrap;
+    @extend .txt-headline3;
+  }
+}
+
 .menu {
-  height: $menu-h * 1px;
-  width: $menu-w * 1px;
+  height: $header_h-desktop * 1px;
+  width: $aside_w-desktop * 1px;
   position: absolute;
   top: 0;
   right: 0;
-  z-index: 1;
+  z-index: $menu_zIndex;
   background-color: $primary;
-  &:hover {
-    span,
-    span::after {
-      width: 55px;
-    }
-  }
+
   span {
     display: block;
     position: absolute;
     top: 50%;
-    left: 33%;
+    left: 27%;
     transform: translateY(-50%);
-    width: 48px;
+    width: 35px;
     height: 1px;
     background: $white;
-    transition: all 0.25s;
+    opacity: 1;
+    transition: all 0.25s ease-in-out;
     &::before,
     &::after {
       position: absolute;
       content: "";
       height: 1px;
       background: $white;
-      transition: all 0.25s;
+      transition: all 0.25s ease-in-out;
     }
     &::before {
-      width: 57px;
-      top: -15px;
+      width: 40px;
+      top: -10px;
     }
     &::after {
-      width: 43px;
-      bottom: -15px;
+      width: 30px;
+      bottom: -10px;
+    }
+  }
+
+  &:hover {
+    span,
+    span::after {
+      width: 40px;
+    }
+  }
+
+  &.active {
+    span {
+      background: transparent;
+      transition: all 0.25s ease-in-out;
+      &::before {
+        width: 45px;
+        top: 0;
+        transform: rotate(-25deg);
+      }
+      &::after {
+        width: 45px;
+        bottom: 0;
+        transform: rotate(25deg);
+      }
     }
   }
 }
@@ -175,23 +250,131 @@ const toggleMenu = () => {
   top: 0;
   left: 0;
   width: 100%;
-  background-color: $white;
-  &__list {
-    position: relative;
-    height: 100vh;
+  height: 100vh;
+  background-color: $primary;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: $aside_w-desktop * 1px;
+    height: 100%;
+    width: 1px;
+    background: rgba($white, 0.2);
+  }
+  &__inner {
+    width: calc(949 / 1440) * 100%;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
   }
-  &__item {
-    margin: 30px 20px;
+  &-main {
+    margin: 88px 0;
+  }
+  &-list {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: -44px;
+  }
+  &-item {
+    position: relative;
     &-link {
-      color: $primary;
+      position: relative;
+      display: block;
+      padding: 40px 0;
+      @extend .txt-headline2;
+      color: $white;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      &::before,
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 1px;
+      }
+      &::before {
+        width: 0%;
+        background: rgba($white, 1);
+        transition: all 0.5s ease-in;
+      }
+      &::after {
+        width: 100%;
+        background: rgba($white, 0.3);
+      }
+      .arrow {
+        fill: rgba($white, 0.3);
+        transition: all 0.5s ease-in;
+      }
+
+      &:hover {
+        color: $white;
+        &::before {
+          width: 100%;
+          transition: all 0.5s ease-out;
+        }
+        .arrow {
+          fill: $white;
+          transition: all 0.5s ease-out;
+        }
+      }
+    }
+  }
+
+  &-submain {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .socialMedia {
+      &-list {
+        display: flex;
+      }
+      &-item {
+        &:not(:last-child) {
+          margin-right: 33px;
+        }
+        &-link {
+          border: 1px solid $white;
+          border-radius: 50%;
+          .item-icon {
+            fill: $white;
+            path {
+            }
+          }
+        }
+      }
+    }
+    .login {
+      a {
+        display: inline-block;
+        position: relative;
+        @extend .txt-headline4;
+        color: $white;
+        &::after {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          height: 1px;
+          width: 0%;
+          background: $white;
+          transition: all 0.5s ease-in;
+        }
+        &:hover {
+          &::after {
+            width: 100%;
+            transition: all 0.5s ease-out;
+          }
+        }
+      }
     }
   }
 }
-
+// 點擊 Menu動畫
 .Menu {
   &-enter-active,
   &-leave-active {
@@ -216,21 +399,21 @@ const toggleMenu = () => {
   }
 }
 
-.menu-open-bar {
-  width: 100vw;
-  height: 50vh;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: $primary;
-  z-index: 101;
-  transform: translate3d(0, -40vh, 0);
-  visibility: hidden;
-  &.toggle {
-    visibility: visible;
-    transform: translate3d(0, 100vh, 0);
-    transition: transform 0.8s;
-    transition-timing-function: $easeInOutQuart;
-  }
-}
+//.menu-open-bar {
+//  width: 100vw;
+//  height: 50vh;
+//  position: absolute;
+//  top: 0;
+//  left: 0;
+//  background-color: $secondary;
+//  z-index: 101;
+//  transform: translate3d(0, -40vh, 0);
+//  visibility: hidden;
+//  &.toggle {
+//    visibility: visible;
+//    transform: translate3d(0, 100vh, 0);
+//    transition: transform 0.8s;
+//    transition-timing-function: $easeInOutQuart;
+//  }
+//}
 </style>
