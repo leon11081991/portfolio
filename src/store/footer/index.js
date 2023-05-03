3756cb977e016759bbceeebb6e5cd539b8a21fae
Leon Chou <iamyiii0328@gmail.com>
2023-05-03 18:46:30 +0800

    vuex- footer module

---
+++ src/store/footer/index.js
@@ -0,0 +1,31 @@
+export const footer = {
+  state() {
+    return {
+      information: [
+        {
+          title: "Gmail",
+          link: "mailto:iamyiii0328@gmail.com",
+          target: "_blank",
+        },
+        {
+          title: "0937-016454",
+          link: "tel:0937016454",
+          target: "",
+        },
+        {
+          title: "Resume Here",
+          link: "#",
+          target: "_blank",
+        },
+        {
+          title: "Designer - Lillian Lin",
+          link: "https://material-hellebore-043.notion.site/e99a7269f5b94db38d55ec6266730065?v=5e9bc2de5bc24512873883ac3f6f43f7",
+          target: "_blank",
+        },
+      ],
+    };
+  },
+  mutations: {},
+  getters: {},
+  actions: {},
+};
