4eb8e356ae3fc224206976b9e71602020f88c4d6
Leon Chou <iamyiii0328@gmail.com>
2023-05-03 18:44:14 +0800

    firebase init

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore/lite";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnVJ1h16rdyPILyf9a5t40lkiXCgff6dQ",
+  authDomain: "leon-s-portfolio-8ebf6.firebaseapp.com",
+  projectId: "leon-s-portfolio-8ebf6",
+  storageBucket: "leon-s-portfolio-8ebf6.appspot.com",
+  messagingSenderId: "693938429578",
+  appId: "1:693938429578:web:93788d1b1e2a578f669773",
+  measurementId: "G-X9ELSWM8BB",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(firebaseApp);
+
+// Timestamp
+const timestamp = serverTimestamp();
+
+export { auth, db, timestamp };
