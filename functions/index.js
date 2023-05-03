0ac4684723de95e4c5f5be428186ea2a12ebe55f
Leon Chou <iamyiii0328@gmail.com>
2023-05-03 18:45:21 +0800

    firebase tools kit

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// Write and deploy the callable function
+// https://firebase.google.com/docs/functions/callable
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
+    })
+    .then(() => {
+      return {
+        message: `Success! ${data.email} has been an admin.`,
+      };
+    });
+});
