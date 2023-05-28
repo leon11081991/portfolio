3ae9c4f888b82647e046ef1a568976499d6767a3
Leon Chou <iamyiii0328@gmail.com>
2023-05-28 17:03:33 +0800

    Component - Logo finished

---
+++ src/components/Base/Logo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="logo-wrapper">
+    <h1 class="logo">
+      <router-link class="siteName" :to="{ name: 'Home' }">
+        <Logo class="logoIcon" />
+      </router-link>
+    </h1>
+  </div>
+</template>
+
+<script setup>
+import Logo from "@assets/logo.svg";
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  &-wrapper {
+    position: relative;
+    height: $header_h-desktop * 1px;
+    display: flex;
+    align-items: center;
+    &.login {
+      position: absolute;
+      top: 0;
+    }
+  }
+  .logoIcon {
+    margin-right: 6px;
+  }
+  .siteName {
+    @extend .txt-headline3;
+    color: $primary;
+  }
+}
+</style>
