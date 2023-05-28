aa8f0bcc8566b7959193cfb42bf839eb9da3bf94
Leon Chou <iamyiii0328@gmail.com>
2023-05-28 17:00:14 +0800

    Component - Page Heading finished

---
+++ src/components/Aside/PageHeading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-heading">
+    <h2 class="page-heading-txt">{{ currentRouteName }}</h2>
+  </div>
+</template>
+
+<script setup>
+// IMPORT NECESSARY
+
+// VARIABLE NECESSARY
+const props = defineProps(["currentRouteName"]);
+</script>
+
+<style lang="scss" scoped>
+.page-heading {
+  margin: 1rem 0 2.5rem 0;
+  &-txt {
+    color: $primary;
+  }
+}
+</style>
