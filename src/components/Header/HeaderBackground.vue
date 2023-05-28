6d1ff29ee3a3b116d997f2433fa838c936c8b019
Leon Chou <iamyiii0328@gmail.com>
2023-05-28 17:04:49 +0800

    Component - Header Background finished

---
+++ src/components/Header/HeaderBackground.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-bg">
+    <div class="layout"></div>
+    <div class="layout blank"></div>
+  </div>
+</template>
+
+<script setup></script>
+<style lang="scss">
+.header-bg {
+  position: absolute;
+  width: calc(100% - $aside_w-desktop * 1px);
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  .layout {
+    position: relative;
+    display: block;
+    border-right: 1px solid rgba($primary, 0.1);
+  }
+}
+</style>
