52b41ed55ff973663c6ecc904f67578a2b04ef0a
Leon Chou <iamyiii0328@gmail.com>
2023-05-28 17:07:11 +0800

    Component - Icon finished

---
+++ src/components/Base/Icon.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="icon" class="icon" />
+</template>
+
+<script setup>
+// IMPORT NECESSARY
+import { defineAsyncComponent } from "vue";
+// VARIABLE NECESSARY
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+// DATA
+const icon = defineAsyncComponent(() =>
+  import(`../../assets/svg/${props.name}.svg`)
+);
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  display: block;
+  width: 25px;
+  height: 25px;
+  margin: 3px;
+  padding: 5px;
+  &.initial {
+    width: 40px;
+    height: 40px;
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
