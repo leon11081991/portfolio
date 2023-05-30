d4d1884105593a407863cf08b87d0cb95db33cc7
Leon Chou <iamyiii0328@gmail.com>
2023-05-30 11:12:12 +0800

    Page - 404

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseLayout>
+    <div class="mainContent">
+      <div class="page-section page-container">
+        <h1>404 not found</h1>
+        <h2>it seems you're in the wrong page</h2>
+      </div>
+    </div>
+  </BaseLayout>
+</template>
+
+<script setup>
+// IMPORT COMPONENT
+import BaseLayout from "@components/Layout/BaseLayout.vue";
+</script>
+
+<style lang="scss" scoped></style>
