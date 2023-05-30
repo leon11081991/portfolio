b4e64f97ba51773b9cde5797cbde2f57242767a8
Leon Chou <iamyiii0328@gmail.com>
2023-05-31 04:24:04 +0800

    Component - Transition slide down to down

---
+++ src/components/Transition/slideDownToDown.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <Transition name="slideDownToDown" @enter="onEnter" @leave="onLeave"> -->
+  <Transition name="slide">
+    <slot></slot>
+    <!-- pass down slot content -->
+  </Transition>
+</template>
+
+<script>
+import { Transition, TransitionGroup } from "vue";
+
+export default {
+  components: {
+    Transition,
+    TransitionGroup,
+  },
+  mounted() {
+    console.log("Transition mounted");
+  },
+};
+</script>
+
+<style lang="scss">
+.slide {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.7s;
+    transition-timing-function: $easeInOutQuart;
+  }
+  &-enter {
+    &-from {
+      transform: translateY(-100%);
+    }
+    &-to {
+      transform: translateY(0);
+    }
+  }
+  &-leave {
+    &-from {
+      transform: translateY(0);
+    }
+    &-to {
+      transform: translateY(100%);
+    }
+  }
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.7s;
+  transition-timing-function: $easeInOutQuart;
+}
+.slide-enter-from {
+  transform: translateY(-100%);
+}
+.slide-enter-to {
+  transform: translateY(0);
+}
+.slide-leave-from {
+  transform: translateY(0);
+}
+.slide-leave-to {
+  transform: translateY(100%);
+}
+</style>
