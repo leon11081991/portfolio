8470fd19e0ed174ed729b6a45469560f107e559e
Leon Chou <iamyiii0328@gmail.com>
2023-05-30 10:41:51 +0800

    Page - Project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,84 +1,45 @@
 <template>
-  <div class="mainContent">
-    <section class="page-section page-container">
-      <aside class="page-aside">
-        <div class="breadcrumb">
-          <div class="breadcrumb-link">
-            <router-link :to="{ name: 'Home' }">
-              <HomeIcon class="icon-bc" />
-            </router-link>
-          </div>
-          <span class="slash">/</span>
-          <div class="breadcrumb-link">
-            <router-link :to="{ name: project }">Projects</router-link>
-          </div>
+  <div>
+    <Loading v-if="!store.state.project.projectLoaded" />
+    <div class="mainContent" :class="route.name" v-else>
+      <section class="page-section page-container">
+        <PageAside></PageAside>
+        <div class="page-content">
+          <ul class="project-list">
+            <ProjectItem
+              v-for="(project, index) in projectList"
+              :key="project.projectTitle"
+              :project="project"
+            ></ProjectItem>
+          </ul>
         </div>
-        <div class="page-heading">
-          <h2 class="page-heading-txt">Projects</h2>
-        </div>
-      </aside>
-      <div class="page-content">
-        <ul class="project-list">
-          <ProjectItem
-            v-for="(project, index) in projects"
-            :key="index"
-            :project="project"
-          ></ProjectItem>
-        </ul>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
-<script setup>
-import HomeIcon from "@assets/icons/home.svg";
 
+<script setup>
+// IMPORT NECESSARY
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+// VARIABLE NECESSARY
+const store = useStore();
+const route = useRoute();
+
+// IMPORT COMPONENT
+import Loading from "@components/Base/Loading.vue";
+import PageAside from "@components/Aside/PageAside.vue";
 import ProjectItem from "@components/Project/ProjectItem.vue";
 
-import { ref } from "vue";
+// DATA
+//const loading = computed(() => store.state.project.projectLoaded);
+const projectList = computed(() => store.state.project.projectList);
 
-const projects = ref([
-  {
-    img: "",
-    title: "綠的國際",
-    link: "",
-  },
-  {
-    img: "",
-    title: "綠的國際-輕品家居",
-    link: "",
-  },
-  {
-    img: "",
-    title: "勵馨基金會",
-    link: "",
-  },
-  {
-    img: "",
-    title: "凱奧藝廊",
-    link: "",
-  },
-  {
-    img: "",
-    title: "Open Vision Technology",
-    link: "",
-  },
-  {
-    img: "",
-    title: "廖儀婷律師事務所",
-    link: "",
-  },
-  {
-    img: "",
-    title: "北藝大文化資源學院",
-    link: "",
-  },
-  {
-    img: "",
-    title: "北藝大博物館研究所",
-    link: "",
-  },
-]);
+console.log("(Projects.vue) --- SETUP");
+store.dispatch("getProjects");
 </script>
+
 <style lang="scss" scoped>
 .project {
   &-list {
@@ -86,6 +47,10 @@ const projects = ref([
     grid-template-columns: repeat(2, 1fr);
     column-gap: 1.5rem;
     row-gap: 40px;
+
+    @include media-breakpoint-down(sm) {
+      grid-template-columns: repeat(1, 1fr);
+    }
   }
 }
 </style>
