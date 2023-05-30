ba3c2a236f8c87ec731caf8df0215dcb671f2743
Leon Chou <iamyiii0328@gmail.com>
2023-05-30 11:17:47 +0800

    Component - Section heading

---
+++ src/components/Base/SectionHeading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="section-heading">
+    <h3 class="section-heading-txt">{{ heading }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionHeading",
+  props: ["heading"],
+};
+</script>
+
+<style lang="scss" scoped>
+.section-heading {
+  margin-bottom: 1rem;
+  &-txt {
+    @extend .txt-headline4;
+  }
+}
+</style>
