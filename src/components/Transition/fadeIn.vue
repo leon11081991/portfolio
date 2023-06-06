5adf5c04ce3f7411651920083ac4c322ae288eab
Leon Chou <iamyiii0328@gmail.com>
2023-06-06 08:34:10 +0800

    Component - Transition fade in

---
+++ src/components/Transition/fadeIn.vue
@@ -0,0 +1,32 @@
+<template>
+  <Transition name="fadeIn">
+    <slot></slot>
+  </Transition>
+</template>
+
+<script setup>
+import { Transition, onMounted } from "vue";
+
+onMounted(() => {
+  console.log("Transition onMounted");
+});
+</script>
+
+<style lang="scss">
+.fadeIn {
+  &-enter-active,
+  &-leave-active {
+    transition: opacity 2s ease;
+  }
+  &-enter {
+    &-from {
+      opacity: 0;
+    }
+  }
+  &-leave {
+    &-to {
+      opacity: 1;
+    }
+  }
+}
+</style>
